f229d76322195e37a4c50dfa2de08242907a77b4
Enoch <madehinenoch@gmail.com>
2022-08-09 11:30:53 -0700

    initial setup

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - prints anything passed to it
+ * @format - format string
+ *
+ * Return - the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
